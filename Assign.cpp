e0cbfdb54d434365cf023ddc16ec73a4c5c3c27f
Ayman Saad <aymansaadhack@gmail.com>
2019-10-31 22:57:29 +0200

    Update Assign.cpp

--- Assign.cpp
+++ Assign.cpp
@@ -1,5 +1,31 @@
-Class OS{
+class product
+{ 
+    
+    public: 
 
-Public:
+    string productName; 
 
-}
+    void printname() 
+    { 
+       cout << Product Name is: " << productName; 
+    } 
+}; 
+  
+
+
+
+class Product
+{ 
+    
+public: 
+    virtual void printName() = 0; 
+    
+}; 
+  
+ 
+class Derived: public Product
+{ 
+     
+public: 
+    void printName() { cout << "printName() called"; } 
+}; 
